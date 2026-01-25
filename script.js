e5b0cfebf1b5ec965bafc061c7e266a855693fb9
Bilal-Qadir-webDeveolper <bhaiprogrammer26@gmail.com>
2026-01-24 17:25:44 -1200

    script complete

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    if (display.value === "Error") display.value = "";
+    display.value += input;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function clearDisplay() {
+    display.value = "";
+}
